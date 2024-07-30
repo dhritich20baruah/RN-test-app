cb043c5299a57e41273a0e3d3045b2f9db6b435e
Dhriti <dhriti.baruah20@gmail.com>
2024-07-30 08:05:41 +0530

    Create and read done

--- App.js
+++ App.js
@@ -1,10 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
+import { SQLiteProvider, useSQLiteContext } from "expo-sqlite";
+
+const initializeDb = async (db) => {
+  try {
+    await db.execAsync(`
+      PRAGMA journal_mode = WAL;
+      CREATE TABLE IF NOT EXISTS testTable2 (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, figure INTEGER);
+      `);
+    console.log("DB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default function App() {
   return (
+    <SQLiteProvider databaseName="testapp.db" onInit={initializeDb}>
+      <Todos />
+    </SQLiteProvider>
+  );
+}
+
+export function Todos() {
+  const db = useSQLiteContext();
+
+  const [figures, setFigures] = useState("");
+  const [prevReadings, setPrevReadings] = useState([]);
+
+  useEffect(() => {
+    async function fetchFigures() {
+      const result = await db.getAllAsync("SELECT * FROM testTable2");
+      setPrevReadings(result);
+      console.log(result);
+    }
+    fetchFigures();
+  }, []);
+
+  const handleSubmit = async () => {
+    const figureValue = parseFloat(figures);
+    if (isNaN(figureValue)) {
+      alert("Please enter a valid number");
+      return;
+    }
+
+    let dateString = new Date().toISOString();
+    let date = dateString
+      .slice(0, dateString.indexOf("T"))
+      .split("-")
+      .reverse()
+      .join("-");
+    try {
+      await db.runAsync("INSERT INTO testTable2 (date, figure) values (?, ?)", [
+        date,
+        figureValue,
+      ]);
+      Alert.alert("Data added")
+      let lastReading = [...prevReadings];
+      lastReading.push({
+        date: date,
+        figure: figureValue,
+      });
+      setPrevReadings(lastReading);
+      setFigures("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>TEST APP</Text>
+      <TextInput
+        style={{ margin: 10, borderColor: "black", borderWidth: 1, padding: 5 }}
+        keyboardType="numeric"
+        value={figures}
+        onChangeText={setFigures}
+        placeholder="0"
+      />
+      <Button title="submit" onPress={handleSubmit} color="lightgreen" />
+      <View>
+        {prevReadings.map((item, index) => {
+          return (
+            <View
+              key={index}
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                width: "80%",
+              }}
+            >
+              <Text>{item.date}</Text>
+              <Text>{item.figure}</Text>
+            </View>
+          );
+        })}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +106,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    marginVertical: 30,
   },
 });
---
+++ components/Chart.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { LineChart } from 'react-native-chart-kit';
+import { Dimensions } from 'react-native';
+
+const screenWidth = Dimensions.get('window').width;
+
+const Chart = ({data}) => {
+  return (
+    <LineChart
+      data={data}
+      width={screenWidth}
+      height={300}
+      verticalLabelRotation={30}
+      yAxisLabel=""
+      chartConfig={{
+        backgroundColor: '#ffffff',
+        backgroundGradientFrom: '#fffeee',
+        backgroundGradientTo: 'green',
+        decimalPlaces: 0, // optional, defaults to 2dp
+        color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+        labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+        style: {
+          borderRadius: 16,
+        },
+        propsForDots: {
+          r: '6',
+          strokeWidth: '2',
+          stroke: '#800000'
+        }
+      }}
+      bezier
+      style={{
+        marginVertical: 8,
+        borderRadius: 16
+      }}
+    />
+  );
+};
+
+export default Chart;
